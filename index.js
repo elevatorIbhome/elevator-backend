8c42a9535f16e04c89f3822b7645bd4b8e24ae7d
jawadibrahim <jawadibrahimshopnil01@gmail.com>
2025-12-14 19:38:22 +0600

    fixed: routing issue

--- index.js
+++ index.js
@@ -39,22 +39,27 @@ app.use((req, res, next) => {
 });
 
 const verifyFirebaseToken = async (req, res, next) => {
-    const idToken = req.headers.authorization?.split('Bearer ')[1];
-
-    if (!idToken) {
-        console.log("token not found");
-        return res.status(401).send({ error: 'Unauthorized: No token provided.' });
-    }
-
     try {
-        const decodedToken = await admin.auth().verifyIdToken(idToken);
-        
-        req.user = decodedToken; 
-        
-        next();
+        const idToken = req.headers.authorization?.split('Bearer ')[1];
+
+        if (!idToken) {
+            console.log("token not found");
+            return res.status(401).send({ error: 'Unauthorized: No token provided.' });
+        }
+
+        try {
+            const decodedToken = await admin.auth().verifyIdToken(idToken);
+            
+            req.user = decodedToken; 
+            
+            next();
+        } catch (error) {
+            console.error("Error verifying Firebase token:", error);
+            return res.status(401).send({ error: 'Unauthorized: Invalid token.' });
+        }
     } catch (error) {
-        console.error("Error verifying Firebase token:", error);
-        return res.status(401).send({ error: 'Unauthorized: Invalid token.' });
+        console.error("Error fetching users:", error);
+        res.status(500).json({ message: "Internal server error" });
     }
 };
 
@@ -254,9 +259,8 @@ async function run() {
 
         // GET: Get a single plan by planId
         app.get("/plans/:planId", async (req, res) => {
-            const planId = req.params.planId;
-
             try {
+                const planId = req.params.planId;
                 const plan = await plansCollection.findOne({ planId });
 
                 if (!plan) {
@@ -273,98 +277,108 @@ async function run() {
 
         // get patment intent 
         app.post("/create-payment-intent", verifyFirebaseToken, async (req, res) => {
-            const { planId } = req.body
+            try {
+                const { planId } = req.body
 
-            const userEmail = req.user.email;
+                const userEmail = req.user.email;
 
-            const plan = await plansCollection.findOne({ planId });
-            if (!plan) {
-                return res.status(400).send({
-                    success: false,
-                    error: "invalid plan"
+                const plan = await plansCollection.findOne({ planId });
+                if (!plan) {
+                    return res.status(400).send({
+                        success: false,
+                        error: "invalid plan"
+                    });
+                }
+
+                const planPriceORE = Math.round(parseFloat(plan.price) * 100); 
+
+                const paymentIntent = await stripe.paymentIntents.create({
+                    amount: planPriceORE,
+                    currency: "dkk",
+                    metadata: { userEmail, planId },
                 });
+
+                res.send({
+                    clientSecret: paymentIntent.client_secret,
+                });
+            } catch (error) {
+                console.error("Error fetching users:", error);
+                res.status(500).json({ message: "Internal server error" });
             }
-
-            const planPriceORE = Math.round(parseFloat(plan.price) * 100); 
-
-            const paymentIntent = await stripe.paymentIntents.create({
-                amount: planPriceORE,
-                currency: "dkk",
-                metadata: { userEmail, planId },
-            });
-
-            res.send({
-                clientSecret: paymentIntent.client_secret,
-            });
         });
 
         // webhook for strpe to get payment info
         app.post('/webhook', express.raw({type: 'application/json'}), async(req, res) => {
-            let event = req.body;
-            
-            const endpointSecret = process.env.STRIPE_WEBHOOK_KEY;
-
-            if (endpointSecret) {
-                const signature = req.headers['stripe-signature'];
-                try {
-                event = stripe.webhooks.constructEvent(
-                    req.body,
-                    signature,
-                    endpointSecret
-                );
-                } catch (err) {
-                console.log(`Webhook signature verification failed.`, err.message);
-                return res.sendStatus(400);
-                }
-            }
-
-            if (event.type === 'payment_intent.succeeded') {
-                const paymentIntent = event.data.object;
-
-                const transactionID = paymentIntent.id;
-                const existingSubscription = await subscriptionCollection.findOne({ transactionID: transactionID });
-
-                if (existingSubscription) {
-                    console.log(`[Idempotency Check] Payment Intent ${transactionID} already processed. Skipping fulfillment.`);
-                    
-                    return res.status(200).send({ received: true }); 
-                }
-
-                const planId = paymentIntent.metadata.planId;
-
-                const plan = await plansCollection.findOne({ planId });
-
-                const {title, period} = plan
+            try {
+                let event = req.body;
                 
-                const newSubscription = {
-                    title,
-                    planId,
-                    period,
-                    amount: paymentIntent.amount,
-                    email: paymentIntent.metadata.userEmail,
-                    buyingDate: new Date().toISOString(),
-                    expireDate: calculateExpireDate(period),
-                    createdAt: new Date().toISOString(),
-                    status: "active",
-                    transactionID
-                };
+                const endpointSecret = process.env.STRIPE_WEBHOOK_KEY;
 
-                const result = await subscriptionCollection.insertOne(newSubscription);
+                if (endpointSecret) {
+                    const signature = req.headers['stripe-signature'];
+                    try {
+                    event = stripe.webhooks.constructEvent(
+                        req.body,
+                        signature,
+                        endpointSecret
+                    );
+                    } catch (err) {
+                    console.log(`Webhook signature verification failed.`, err.message);
+                    return res.sendStatus(400);
+                    }
+                }
 
-                // Send to Google Apps Script
-                fetch("https://script.google.com/macros/s/AKfycbzrlzWNKXh78Ke3nPMwPVPlwQfwsi7zxakamZ0NplJ1hCJN-8kaih-hUYG8RRMMMEUCtA/exec", {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify(newSubscription)
-                })
-                .then(res => console.log("Saved to Google Sheet"))
-                .catch(err => console.error("Error saving to Google Sheet:", err));
+                if (event.type === 'payment_intent.succeeded') {
+                    const paymentIntent = event.data.object;
 
-            }else{
-                console.log(`Unhandled event type ${event.type}.`);
+                    const transactionID = paymentIntent.id;
+                    const existingSubscription = await subscriptionCollection.findOne({ transactionID: transactionID });
+
+                    if (existingSubscription) {
+                        console.log(`[Idempotency Check] Payment Intent ${transactionID} already processed. Skipping fulfillment.`);
+                        
+                        return res.status(200).send({ received: true }); 
+                    }
+
+                    const planId = paymentIntent.metadata.planId;
+
+                    const plan = await plansCollection.findOne({ planId });
+
+                    const {title, period} = plan
+                    
+                    const newSubscription = {
+                        title,
+                        planId,
+                        period,
+                        amount: paymentIntent.amount,
+                        email: paymentIntent.metadata.userEmail,
+                        buyingDate: new Date().toISOString(),
+                        expireDate: calculateExpireDate(period),
+                        createdAt: new Date().toISOString(),
+                        status: "active",
+                        transactionID
+                    };
+
+                    const result = await subscriptionCollection.insertOne(newSubscription);
+
+                    // Send to Google Apps Script
+                    fetch("https://script.google.com/macros/s/AKfycbzrlzWNKXh78Ke3nPMwPVPlwQfwsi7zxakamZ0NplJ1hCJN-8kaih-hUYG8RRMMMEUCtA/exec", {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify(newSubscription)
+                    })
+                    .then(res => console.log("Saved to Google Sheet"))
+                    .catch(err => console.error("Error saving to Google Sheet:", err));
+
+                }else{
+                    console.log(`Unhandled event type ${event.type}.`);
+                }
+                
+                res.send();
+            } catch (error) {
+                console.error("Error fetching users:", error);
+                res.status(500).json({ message: "Internal server error" });
             }
-            
-            res.send();
 
         });
 
